d72a134a0c1c8c69b0326dba906c1409da5a6997
hossein1376 <hosseinyazdani1376@gmail.com>
2023-08-07 20:05:28 +0330

    Add user error helpers

--- internal/Errors/Users.go
+++ internal/Errors/Users.go
@@ -1,5 +1,9 @@
 package Errors
 
+import (
+	"fmt"
+)
+
 const UserNotFoundText = "user was not found"
 
 type UserNotFound struct {
@@ -11,6 +15,35 @@ func (UserNotFound) Error() string {
 	return UserNotFoundText
 }
 
+func (u UserNotFound) GetID() *int {
+	return u.ID
+}
+
+func (u UserNotFound) GetUsername() *string {
+	return u.Username
+}
+
+func (u UserNotFound) WithID() string {
+	if u.ID == nil {
+		return UserNotFoundText
+	}
+	return fmt.Sprintf("user with id '%v' was not found", u.ID)
+}
+
+func (u UserNotFound) WithUsername() string {
+	if u.Username == nil {
+		return UserNotFoundText
+	}
+	return fmt.Sprintf("user with username '%v' was not found", u.Username)
+}
+
+func (u UserNotFound) WithIDWithUsername() string {
+	if u.ID == nil || u.Username == nil {
+		return UserNotFoundText
+	}
+	return fmt.Sprintf("user with id '%v' and username '%v' was not found", u.ID, u.Username)
+}
+
 const UserDisabledText = "user is disabled"
 
 type UserDisabled struct {
@@ -20,4 +53,33 @@ type UserDisabled struct {
 
 func (UserDisabled) Error() string {
 	return UserDisabledText
+}
+
+func (u UserDisabled) GetID() *int {
+	return u.ID
+}
+
+func (u UserDisabled) GetUsername() *string {
+	return u.Username
+}
+
+func (u UserDisabled) WithID() string {
+	if u.ID == nil {
+		return UserDisabledText
+	}
+	return fmt.Sprintf("user with id '%v' is disabled", u.ID)
+}
+
+func (u UserDisabled) WithUsername() string {
+	if u.Username == nil {
+		return UserDisabledText
+	}
+	return fmt.Sprintf("user with username '%v' is disabled", u.Username)
+}
+
+func (u UserDisabled) WithIDWithUsername() string {
+	if u.ID == nil || u.Username == nil {
+		return UserDisabledText
+	}
+	return fmt.Sprintf("user with id '%v' and username '%v' is disabled", u.ID, u.Username)
 }
