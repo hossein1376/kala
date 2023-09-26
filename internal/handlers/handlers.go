2ee7c92505ce10bcb8c03c45ee113995e7f0931c
Hossein Yazdani <hosseinyazdani1376@gmail.com>
2023-09-26 17:07:40 +0330

    Mover handlers to internal folder

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,7 +1,7 @@
 package handlers
 
 import (
-	"github.com/hossein1376/kala/cmd"
+	"github.com/hossein1376/kala/config"
 	"github.com/hossein1376/kala/internal/response"
 	"github.com/hossein1376/kala/pkg/Json"
 )
@@ -10,14 +10,14 @@ import (
 type JsonHandler = Json.Jsoniter
 
 type handler struct {
-	*cmd.Application
+	*config.Application
 	*response.Response
 	JsonHandler
 }
 
-func NewHandlers(app *cmd.Application) *handler {
+func NewHandlers(app *config.Application) *handler {
 	return &handler{
-		Application: &cmd.Application{
+		Application: &config.Application{
 			Config: app.Config,
 			Logger: app.Logger,
 			Models: app.Models,
